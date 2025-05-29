dfa0a672aa20abe12c9bf8cd6fe9083f9deb8a05
Sofía Santos García <sofiasantos@MacBook-Air-de-Sofia.local>
2025-05-29 18:26:30 +0200

    fix rows and columns exercise 4

--- RandomNumbers.java
+++ RandomNumbers.java
@@ -3,6 +3,8 @@ import java.util.Random;
 public class RandomNumbers {
 
     public static void main(String[] args) {
+     //Escribe un programa que genere un array de 2 dimensiones de 100 filas y 10 columnas  con números aleatorios del 0 al 99, a través de un bucle anidado (Si no te suenan estos conceptos, ¡búscalos!)
+
         int filas = 100;
         int columnas = 10;
         int[][] numeros = new int[filas][columnas];
@@ -13,11 +15,12 @@ public class RandomNumbers {
         }
         print2DArray(numeros);
     }
+
     /**
      * Function name: randomNumber
      *
      * @return (int)
-     *
+     * <p>
      * Inside the function:
      * 1. generate random numbers from 0 to 99
      */
@@ -25,16 +28,21 @@ public class RandomNumbers {
         Random rand = new Random();
         return rand.nextInt(100);
     }
+
     /**
      * Function name: print2DArray
      *
      * @param integers (2D array int)
-     *
-     * Inside the function:
-     * 1. nested loop that prints a 2D array using the randomNumber function
+     *                 <p>
+     *                 Inside the function:
+     *                 1. nested loop that prints a 2D array using the randomNumber function
      */
     public static void print2DArray(int[][] integers) {
         for (int i = 0; i < integers.length; i++) {
             for (int j = 0; j < integers[i].length; j++) {
                 System.out.print(integers[i][j] + "\t");
             }
+            System.out.println();
+        }
+    }
+}
